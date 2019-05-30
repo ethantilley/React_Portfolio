574b0415335da92a86c6716bbc77a0ab7b1df4c8
Ethan Tilley <1008464@student.sae.edu.au>
2019-05-30 21:10:36 +1000

    Implemented project sections with: React, Unity and Xamarin project catagories!

--- src/Components/projects.js
+++ src/Components/projects.js
@@ -8,11 +8,55 @@
 
 
 import React, { Component } from 'react';
+import { Tabs, Tab, Grid, Cell } from 'react-mdl';
 
 class Projects extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { activeTab: 0 };
+  }
+
+
+  toggleCategories() {
+    if(this.state.activeTab === 0) {
+      return(
+      <div><h1>Unity!!!</h1></div>
+      )
+    } else if(this.state.activeTab === 1) {
+      return(
+      <div><h1>React!!!</h1></div>
+      )
+    } else if(this.state.activeTab === 2) {
+      return(
+      <div><h1>Xamarin!!!</h1></div>
+      )
+    }
+  }
+
   render() {
     return(
-      <div> <h1>Projects Page</h1> </div>
+      <div className="project-category-tabs">
+        <Tabs activeTab={this.state.activeTab} onChange={(tabId) =>
+           this.setState({ activeTab: tabId })} ripple>
+
+          <Tab>Unity</Tab>
+          <Tab>React</Tab>
+          <Tab>Xamarin</Tab>
+
+        </Tabs>
+
+        <section className="projects-grid">
+          <Grid className="projects-grid">
+
+            <Cell col={12}>
+
+            <div className="project-content"> {this.toggleCategories()}</div>
+
+            </Cell>
+
+          </Grid>
+        </section>
+      </div>
     )
   }
 }
