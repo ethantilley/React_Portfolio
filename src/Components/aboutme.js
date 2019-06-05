7822c599405699aacfce91e4fecef1d3d3789708
Ethan Tilley <1008464@student.sae.edu.au>
2019-06-06 00:04:18 +1000

    Made a start on blocking out the contact-me/about-me page

--- src/Components/aboutme.js
+++ src/Components/aboutme.js
@@ -7,11 +7,50 @@
 
 
 import React, { Component } from 'react';
-
+import { Grid, Cell,ListItem, List, ListItemContent } from 'react-mdl';
 class AboutMe extends Component {
   render() {
     return(
-      <div> <h1>AboutMe Page</h1> </div>
+      <div className = "contact-body">
+        <Grid className= "contact-grid">
+          <Cell col = {6}>
+            <h2> About Me </h2>
+            <img
+            src = {require('../Media/ProfilePic.jpg')}
+            alt = "ProfilePic"
+            className = "ProfilePic"
+            style={{height: '250px'}}
+            />
+            <h2> Ethan Tilley </h2>
+            <p style = {{width: '75%', margin: 'auto', paddingTop: "1em"}} >
+            Hello! Im Ethan Tilley, I’m a developer based in Brisbane previously studied Game Development at SAE majoring in Programming, now actively looking for work. Although I love all kinds of programming, I like to focus my spare time on AI programming.
+            </p>
+
+
+          </Cell>
+          <Cell col = {6}>
+            <h2>Contact Me</h2>
+            <hr/>
+            <div className="contact-list">
+
+              <List style={{align: 'center'}}>
+                <ListItem>
+                  <ListItemContent style = {{fontSize: '25px', fontFamily: 'Anton'}}>
+                    <i className="fa fa-phone-square" aria-hidden=true />
+                    0423448291
+                  </ListItem>
+                <ListItem>
+                  <ListItemContent icon="person">Aaron Paul</ListItemContent>
+                </ListItem>
+                <ListItem>
+                  <ListItemContent icon="person">Bob Odenkirk</ListItemContent>
+                </ListItem>
+              </List>
+            </div>
+          </Cell>
+        </Grid>
+
+      </div>
     )
   }
 }
--- src/Components/landingpage.js
+++ src/Components/landingpage.js
@@ -51,25 +51,26 @@ class Landing extends Component {
 				<p> C# | C++ | CSS | HTML | JavaScript | Python </p>
 					<div className="social-links">
 
-					 /* LinkedIn */
-					 <a href="http://google.com" rel="noopener noreferrer" target="_blank">
-						 <i className="fa fa-linkedin-square" aria-hidden="true" />
-					 </a>
+					{/* LinkedIn */}
+           <a href="http://google.com" rel="noopener noreferrer" target="_blank"
+             <i className="fa fa-linkedin-square" aria-hidden="true" />
+           />
 
-					 /* Github */
-					 <a href="http://google.com" rel="noopener noreferrer" target="_blank">
-						 <i className="fa fa-github-square" aria-hidden="true" />
-					 </a>
+           {/* Github */}
+           <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+             <i className="fa fa-github-square" aria-hidden="true" />
+           </a>
 
-					 /* Freecodecamp */
-					 <a href="http://google.com" rel="noopener noreferrer" target="_blank">
-						 <i className="fa fa-free-code-camp" aria-hidden="true" />
-					 </a>
+           {/* Freecodecamp */}
+           <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+             <i className="fa fa-free-code-camp" aria-hidden="true" />
+           </a>
 
-					 /* Youtube */
-					 <a href="http://google.com" rel="noopener noreferrer" target="_blank">
-						 <i className="fa fa-youtube-square" aria-hidden="true" />
-					 </a>
+           {/* Youtube */}
+           <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+             <i className="fa fa-youtube-square" aria-hidden="true" />
+           </a>
+
 
 				 </div>
 
